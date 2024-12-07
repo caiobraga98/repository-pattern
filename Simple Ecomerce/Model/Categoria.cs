46fa62e3fbac6d405cabf5d15ea9e6b1d7e10f40
caio.braga <ext.caio.ferraz@zamp.com.br>
2024-12-07 09:46:59 -0300

    creation of model classes

---
+++ Simple Ecomerce/Model/Categoria.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Simple_Ecomerce.Model
+{
+    public class Categoria
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        public List<Produto> Produtos { get; set; }
+    }
+}
---
+++ Simple Ecomerce/Model/Produto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Simple_Ecomerce.Model
+{
+    public class Produto
+    {
+        [Key]
+        public int id { get; set; }
+
+        public string nome { get; set; }
+
+        public string descricao { get; set; }
+
+        public double preco { get; set; }
+    }
+}
